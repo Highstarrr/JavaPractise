375995a226ef622e34b2b9dcbbbb7c0130bf69a1
Highstarrr <2508413985@qq.com>
2020-03-22 17:02:00 +0800

    两数求和练习

---
+++ Sum.java
@@ -0,0 +1,11 @@
+package practise02;
+import java.util.Scanner;
+public class Sum {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int x1 = in.nextInt();
+		int x2 = in.nextInt();
+		int x3 = x1 + x2;
+		System.out.println(x3);
+	}
+}
